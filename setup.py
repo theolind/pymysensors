35b87d880147a34107da0d40cb815d75e6cb4af7
theolind <theodor.lindquist@gmail.com>
2015-08-15 12:15:47 +0200

    Updated version number 0.1 -> 0.2

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='pymysensors',
-      version='0.1',
+      version='0.2',
       description='Python API for talking to a MySensors gateway',
       url='https://github.com/theolind/pymysensors',
       author='Theodor Lindquist',
