e2f0df6baadd1e8ee8a9f7a5e305af9ea8e131d3
MartinHjelmare <marhje52@kth.se>
2016-03-19 16:43:49 +0100

    Update readme and main.py
    * Update sensor data structure.
    * Add example about sending new values to node, using
      Gateway.set_child_value.
    * Clarify how persistence can be set.
    * Add travis build badge.

--- main.py
+++ main.py
@@ -9,6 +9,6 @@ def event(update_type, nid):
 GATEWAY = mysensors.SerialGateway('/dev/ttyACM0', event, True)
 GATEWAY.debug = True
 GATEWAY.start()
-# To set sensor 2, child 1, sub-type V_LIGHT (= 2), with value 1.
+# To set sensor 1, child 1, sub-type V_LIGHT (= 2), with value 1.
 GATEWAY.set_child_value(1, 1, 2, 1)
 GATEWAY.stop()
