bbc692e8cfcf9ef876ea2f633c839821cc9c36d9
Martin Hjelmare <marhje52@kth.se>
2017-03-25 10:55:13 +0100

    Fix ota with persistence (#87)

--- mysensors/__init__.py
+++ mysensors/__init__.py
@@ -193,7 +193,7 @@ class Gateway(object):
     def _load_pickle(self, filename):
         """Load sensors from pickle file."""
         with open(filename, 'rb') as file_handle:
-            self.sensors = pickle.load(file_handle)
+            self.sensors.update(pickle.load(file_handle))
 
     def _save_json(self, filename):
         """Save sensors to json file."""
@@ -205,7 +205,8 @@ class Gateway(object):
     def _load_json(self, filename):
         """Load sensors from json file."""
         with open(filename, 'r') as file_handle:
-            self.sensors = json.load(file_handle, cls=MySensorsJSONDecoder)
+            self.sensors.update(
+                json.load(file_handle, cls=MySensorsJSONDecoder))
 
     def _save_sensors(self):
         """Save sensors to file."""
