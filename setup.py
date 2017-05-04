627f7897289446f7d35ebd93539a67981d40e198
Theodor Lindquist <theodor.lindquist@gmail.com>
2017-05-04 20:27:52 +0200

    Fixed minor stuff required to publish on pypi

--- setup.py
+++ setup.py
@@ -1,12 +1,15 @@
 """Setup file for mysensors package."""
 from setuptools import setup
 
-setup(name='pymysensors',
-      version='0.9.1',
-      description='Python API for talking to a MySensors gateway',
-      url='https://github.com/theolind/pymysensors',
-      author='Theodor Lindquist',
-      license='MIT',
-      install_requires=['pyserial>=3.1.1', 'crcmod>=1.7', 'IntelHex>=2.1'],
-      packages=['mysensors'],
-      zip_safe=True)
+setup(
+    name='pymysensors',
+    version='0.9.1',
+    description='Python API for talking to a MySensors gateway',
+    url='https://github.com/theolind/pymysensors',
+    author='Theodor Lindquist',
+    author_email='theodor.lindquist@gmail.com',
+    license='MIT',
+    install_requires=['pyserial>=3.1.1', 'crcmod>=1.7', 'IntelHex>=2.1'],
+    packages=['mysensors'],
+    zip_safe=True
+)
