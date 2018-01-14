b806b42a3ff1cd834c487fcf34d68a185964dcc0
Martin Hjelmare <marhje52@kth.se>
2018-01-15 00:23:12 +0100

    Bump version to 0.12.1

--- mysensors/version.py
+++ mysensors/version.py
@@ -1,5 +1,5 @@
 """Store version constants."""
 MAJOR_VERSION = 0
 MINOR_VERSION = 12
-PATCH_VERSION = '0'
+PATCH_VERSION = '1'
 __version__ = '{}.{}.{}'.format(MAJOR_VERSION, MINOR_VERSION, PATCH_VERSION)
