e81eb2671787fb938d78356fb7ca97d6d3c884e7
Martin Hjelmare <marhje52@kth.se>
2018-05-16 12:42:12 +0200

    Clean up get_const

--- mysensors/const.py
+++ mysensors/const.py
@@ -5,20 +5,22 @@ from importlib import import_module
 
 LOADED_CONST = {}
 
+CONST_VERSIONS = {
+    '1.4': 'mysensors.const_14',
+    '1.5': 'mysensors.const_15',
+    '2.0': 'mysensors.const_20',
+    '2.1': 'mysensors.const_21',
+    '2.2': 'mysensors.const_22',
+}
+
 
 def get_const(protocol_version):
     """Return the const module for the protocol_version."""
-    version = protocol_version
-    if parse_ver('1.5') <= parse_ver(version) < parse_ver('2.0'):
-        path = 'mysensors.const_15'
-    elif parse_ver(version) >= parse_ver('2.2'):
-        path = 'mysensors.const_22'
-    elif parse_ver(version) >= parse_ver('2.1'):
-        path = 'mysensors.const_21'
-    elif parse_ver(version) >= parse_ver('2.0'):
-        path = 'mysensors.const_20'
-    else:
-        path = 'mysensors.const_14'
+    path = next((
+        CONST_VERSIONS[const_version]
+        for const_version in sorted(CONST_VERSIONS, reverse=True)
+        if parse_ver(protocol_version) >= parse_ver(const_version)
+    ), 'mysensors.const_14')
     if path in LOADED_CONST:
         return LOADED_CONST[path]
     const = import_module(path)
